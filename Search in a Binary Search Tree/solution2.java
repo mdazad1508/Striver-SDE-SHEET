57320c7d6ec07e8663b5a65d492f4f2c75e43229
mdazad1508 <mdazad15081999@gmail.com>
2023-06-19 02:51:19 +0530

    BST

---
+++ Search in a Binary Search Tree/solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+       if(root==null ) return null;
+
+       TreeNode curr = root;
+
+       while(curr != null){
+
+           if(curr.val == val){
+               return curr;
+           }else if(val < curr.val){
+               curr = curr.left;
+           }else{
+               curr= curr.right;
+           }
+       }
+
+       return null;
+        
+    }
+}
