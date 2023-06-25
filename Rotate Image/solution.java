6cfbd146ee094e5f7ffe06a94bab881fcd61dc16
mdazad1508 <mdazad15081999@gmail.com>
2023-06-25 21:35:56 +0530

    array ii

---
+++ Rotate Image/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int [][] copy = new int[matrix.length][matrix.length];
+
+
+        for(int i=0;i<matrix.length;i++){
+
+            for(int j=0;j<matrix.length;j++){
+
+                copy[j][matrix.length-1-i] = matrix[i][j];
+            }
+        }
+
+         for(int i=0;i<matrix.length;i++){
+
+            for(int j=0;j<matrix.length;j++){
+
+                matrix[i][j] = copy[i][j];
+            }
+        }
+
+        
+
+        
+    }
+}
