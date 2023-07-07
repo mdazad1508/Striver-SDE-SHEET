d200df3fa76c6b4ae203291f71ff6fed0d2f0fba
mdazad1508 <mdazad15081999@gmail.com>
2023-07-07 08:24:05 +0530

    stack

---
+++ next smaller element/sol.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n){
+       
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=arr.size()-1;i>=0;i--){
+           
+            while(!st.isEmpty() && st.peek()>= arr.get(i)){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans.add(-1);
+            }else{
+                ans.add(st.peek());
+            }
+
+            st.push(arr.get(i));
+
+        }
+        
+        Collections.reverse(ans);
+        return ans;
+
+        
+    }
+}
